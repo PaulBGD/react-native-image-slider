70241e1ceddde569dc98c23b8d915490f6ca1899
Paul Sauve <paul@burngames.net>
2016-08-08 16:16:14 -0500

    Make ImageSlider controlled, frictioned, and fix a few bugs

--- ImageSlider.js
+++ ImageSlider.js
@@ -1,8 +1,8 @@
-import React, {
+import React, {Component} from 'react';
+import {
     Image,
     Text,
     View,
-    Component,
     StyleSheet,
     Animated,
     PanResponder,
@@ -47,21 +47,55 @@ export default class ImageSlider extends Component {
         this.state = {
             position: 0,
             height: Dimensions.get('window').width * (4 / 9),
-            left: new Animated.Value(0)
+            left: new Animated.Value(0),
+            scrolling: false,
+            timeout: null
         };
     }
 
     _move(index) {
-        let width = Dimensions.get('window').width;
-        Animated.spring(this.state.left, {toValue: index * -width, friction: 10, tension: 50}).start();
-        this.setState({position: index});
+        const width = Dimensions.get('window').width;
+        const to = index * -width;
+        if (!this.state.scrolling) {
+            return;
+        }
+        Animated.spring(this.state.left, {toValue: to, friction: 10, tension: 50}).start();
+        if (this.state.timeout) {
+            clearTimeout(this.state.timeout);
+        }
+        this.setState({position: index, timeout: setTimeout(() => {
+            this.setState({scrolling: false, timeout: null});
+            if (this.props.onPositionChanged) {
+                this.props.onPositionChanged(index);
+            }
+        }, 500)});
+    }
+
+    _getPosition() {
+        if (typeof this.props.position === 'number') {
+            return this.props.position;
+        }
+        return this.state.position;
+    }
+
+    componentWillReceiveProps(props) {
+        if (props.position !== undefined) {
+            this.setState({scrolling: true});
+            this._move(props.position);
+        }
     }
 
     componentWillMount() {
+        const width = Dimensions.get('window').width;
+
+        if (typeof this.props.position === 'number') {
+            this.state.left.setValue(-(width * this.props.position));
+        }
+
         let release = (e, gestureState) => {
-            let width = Dimensions.get('window').width;
-            let relativeDistance = gestureState.dx / width;
-            let vx = gestureState.vx;
+            const width = Dimensions.get('window').width;
+            const relativeDistance = gestureState.dx / width;
+            const vx = gestureState.vx;
             let change = 0;
 
             if (relativeDistance < -0.5 || (relativeDistance < 0 && vx <= 0.5)) {
@@ -69,12 +103,14 @@ export default class ImageSlider extends Component {
             } else if (relativeDistance > 0.5 || (relativeDistance > 0 && vx >= 0.5)) {
                 change = -1;
             }
-            if (this.state.position === 0 && change === -1) {
+            const position = this._getPosition();
+            if (position === 0 && change === -1) {
                 change = 0;
-            } else if (this.state.position + change >= this.props.images.length) {
-                change = (this.props.images.length) - (this.state.position + change);
+            } else if (position + change >= this.props.images.length) {
+                change = (this.props.images.length) - (position + change);
             }
-            this._move(this.state.position + change);
+            this._move(position + change);
+            return true;
         };
 
         this._panResponder = PanResponder.create({
@@ -87,22 +123,44 @@ export default class ImageSlider extends Component {
             onPanResponderRelease: release,
             onPanResponderTerminate: release,
             onPanResponderMove: (e, gestureState) => {
-                let dx = gestureState.dx;
-                let width = Dimensions.get('window').width;
-                this.state.left.setValue(-(this.state.position * width) + Math.round(dx));
-            }
+                const dx = gestureState.dx;
+                const width = Dimensions.get('window').width;
+                const position = this._getPosition();
+                let left = -(position * width) + Math.round(dx);
+                if (left > 0) {
+                    left = Math.sin(left / width) * (width / 2);
+                } else if (left < -(width * (this.props.images.length - 1))) {
+                    const diff = left + (width * (this.props.images.length - 1));
+                    left = Math.sin(diff / width) * (width / 2) - (width * (this.props.images.length - 1));
+                }
+                this.state.left.setValue(left);
+                if (!this.state.scrolling) {
+                    this.setState({scrolling: true});
+                }
+            },
+            onShouldBlockNativeResponder: () => true
         });
     }
 
+    componentWillUnmount() {
+        if (this.state.timeout) {
+            clearTimeout(this.state.timeout);
+        }
+    }
+
     render() {
-        let width = Dimensions.get('window').width;
-        let height = this.props.height || this.state.height;
+        const width = Dimensions.get('window').width;
+        const height = this.props.height || this.state.height;
+        const position = this._getPosition();
         return (<View>
             <Animated.View
                 style={[styles.container, {height: height, width: width * this.props.images.length, transform: [{translateX: this.state.left}]}]}
                 {...this._panResponder.panHandlers}>
                     {this.props.images.map((image, index) => {
-                        return (<Image key={index} source={{uri: image}} style={[styles.image, {height: height}]}/>)
+                        return (<Image
+                            key={index}
+                            source={{uri: image}}
+                            style={{height: position === index || this.state.scrolling ? height : 0, width}}/>)
                     })}
             </Animated.View>
             <View style={styles.buttons}>
@@ -110,8 +168,11 @@ export default class ImageSlider extends Component {
                     return (<TouchableHighlight
                         key={index}
                         underlayColor="#ccc"
-                        onPress={() => this._move(index)}
-                        style={[styles.button, this.state.position === index && styles.buttonSelected]}>
+                        onPress={() => {
+                            this.setState({scrolling: true});
+                            return this._move(index);
+                        }}
+                        style={[styles.button, position === index && styles.buttonSelected]}>
                             <View></View>
                     </TouchableHighlight>);
                 })}
--- README.md
+++ README.md
@@ -28,11 +28,56 @@ render() {
 To keep the height from shifting, we use a static height.
 If you want to change the height, simply pass a height to the component.
 
+### Props
+
+* `height`: controls the height. By default the height is static, is this if you want the height to change
+* `onPositionChanged`: called when the current position is changed
+* `position`: used for controlled components
+
+### Autoplay Example
+
+```javascript
+class SliderTests extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            position: 1,
+            interval: null
+        };
+    }
+
+    componentWillMount() {
+        this.setState({interval: setInterval(() => {
+            this.setState({position: this.state.position === 2 ? 0 : this.state.position + 1});
+        }, 2000)});
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageSlider
+                    images={[
+                        `http://placeimg.com/640/480/any`,
+                        `http://placeimg.com/640/480/any`,
+                        `http://placeimg.com/640/480/any`,
+                    ]}
+                    position={this.state.position}
+                    onPositionChanged={position => this.setState({position})}/>
+            </View>
+        );
+    }
+}
+```
+
 ## To Do
 
 Please feel free to fork and PR!
 
-* Add an event when the image changes
 * Add the option for custom buttons
 
 ## License
