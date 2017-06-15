34c7738a19d4a25010e14f2f23cf585a631bcf76
Wooooha <lizouzt@gmail.com>
2017-06-15 14:56:51 +0800

    Support slider loop
    Let gallery support picture loop

--- ImageSlider.js
+++ ImageSlider.js
@@ -5,22 +5,16 @@ import {
     View,
     ScrollView,
     StyleSheet,
-    Animated,
     PanResponder,
     TouchableHighlight,
     TouchableOpacity,
     Dimensions
 } from 'react-native';
 
-const reactNativePackage = require('react-native/package.json');
-const splitVersion = reactNativePackage.version.split('.');
-const majorVersion = +splitVersion[0];
-const minorVersion = +splitVersion[1];
-
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        backgroundColor: '#222'
+        backgroundColor: '#f5f5f5'
     },
     buttons: {
         height: 15,
@@ -63,14 +57,23 @@ export default class ImageSlider extends Component {
         }
     }
 
-    _move(index) {
+    _move(index, flag) {
         const isUpdating = index !== this._getPosition();
-        const x = this.state.width * index;
-        if (majorVersion === 0 && minorVersion <= 19) {
-            this._ref.scrollTo(0, x, true); // use old syntax
-        } else {
-            this._ref.scrollTo({x: this.state.width * index, y: 0, animated: true});
+
+        //Since ScrollView not support AnimationEnd event with android.
+        if (index >= this.props.images.length) {
+            setTimeout(function () {
+                this._ref.scrollTo({x: this.state.width * (0 + 1), y: 0, animated: false});
+            }.bind(this), 200);
+        } else if (index < 0) {
+            setTimeout(function () {
+                this._ref.scrollTo({x: this.state.width * (this.props.images.length), y: 0, animated: false});
+            }.bind(this), 200);
         }
+        this._ref.scrollTo({x: this.state.width * (index + 1), y: 0, animated: flag == undefined ? true : flag});
+
+        index = (index < 0 ? (index + this.props.images.length) : index) % this.props.images.length;
+
         this.setState({position: index});
         if (isUpdating && this.props.onPositionChanged) {
             this.props.onPositionChanged(index);
@@ -90,6 +93,10 @@ export default class ImageSlider extends Component {
         }
     }
 
+    componentDidMount() {
+        this._ref.scrollTo({x: this.state.width, y: 0, animated: false});
+    }
+
     componentWillMount() {
         const width = this.state.width;
 
@@ -104,12 +111,9 @@ export default class ImageSlider extends Component {
             } else if (relativeDistance > 0.5 || (relativeDistance > 0 && vx >= 0.5)) {
                 change = -1;
             }
-            const position = this._getPosition();
-            if (position === 0 && change === -1) {
-                change = 0;
-            } else if (position + change >= this.props.images.length) {
-                change = (this.props.images.length) - (position + change);
-            }
+
+            let position = this._getPosition();
+
             this._move(position + change);
             return true;
         };
@@ -134,15 +138,20 @@ export default class ImageSlider extends Component {
         const width = this.state.width;
         const height = this.props.height || this.state.height;
         const position = this._getPosition();
+
+        const images = [].concat(this.props.images);
+        images.push(this.props.images[0]);
+        images.unshift(this.props.images[this.props.images.length - 1]);
+
         return (<View>
             <ScrollView
                 ref={ref => this._onRef(ref)}
-                decelerationRate={0.99}
+                decelerationRate={0.8}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 {...this._panResponder.panHandlers}
                 style={[styles.container, this.props.style, {height: height}]}>
-                {this.props.images.map((image, index) => {
+                {images.map((image, index) => {
                     const imageObject = typeof image === 'string' ? {uri: image} : image;
                     const imageComponent = <Image
                         key={index}
