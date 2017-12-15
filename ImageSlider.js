8a178f2df76e87932ebd850083e33e5d4bc616a8
Todd Wilson <todd@dodgertodd.com>
2017-12-15 13:41:18 -0700

    cleanup

--- ImageSlider.js
+++ ImageSlider.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import {
     Image,
-    Text,
     View,
     ScrollView,
     StyleSheet,
-    Animated,
-    PanResponder,
     TouchableHighlight,
     TouchableOpacity,
     Dimensions
